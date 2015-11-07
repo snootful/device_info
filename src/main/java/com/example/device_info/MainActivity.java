909a9a7a07242e62aa84c513cde9abb8caf37ea5
John Doe <goodbyecrazy@gmail.com>
2015-11-08 00:55:56 +0800

    The initial version

---
+++ src/main/java/com/example/device_info/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.device_info;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ArrayList<String[]> list = new ArrayList<>();
+        list.add(new String[]{"CODENAME", Build.VERSION.CODENAME});
+        list.add(new String[]{"RELEASE", Build.VERSION.RELEASE});
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            list.add(new String[]{"SECURITY_PATCH", Build.VERSION.SECURITY_PATCH});
+        }
+        list.add(new String[]{"The user-visible SDK version of the framework", Integer.toString(Build.VERSION.SDK_INT)});
+        list.add(new String[]{"The name of the underlying board", Build.BOARD});
+        list.add(new String[]{"A build ID string meant for displaying to the user", Build.DISPLAY});
+        list.add(new String[]{""});
+
+
+        ArrayAdapter<String[]> arrayAdapter = new ArrayAdapter<String []>(this,
+                android.R.layout.simple_list_item_2,
+                android.R.id.text1,
+                list){
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+                TextView text2 = (TextView) view.findViewById(android.R.id.text2);
+
+                String[] entry = list.get(position);
+                text1.setText(entry[0]);
+                text2.setText(entry[1]);
+
+                return view;
+            }
+        };
+
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(arrayAdapter);
+    }
+}
