8f9fb5fb244d59384559fbd1ea904d8ed2f12421
John Doe <goodbyecrazy@gmail.com>
2015-11-10 21:06:21 +0800

    Add  something about battery.

---
+++ src/main/java/com/example/device_info/BatteryActivity.java
@@ -0,0 +1,122 @@
+package com.example.device_info;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class BatteryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_battery);
+
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = registerReceiver(null, ifilter);
+        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        int status2 = BatteryManager.BATTERY_STATUS_FULL;
+        int status3 = batteryStatus.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0);
+        float temperature = ((float) status3) / 10;
+        int status4 = batteryStatus.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0);
+        float voltage = ((float) status4) /1000;
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, 0);
+        int status5 = batteryStatus.getIntExtra(BatteryManager.EXTRA_HEALTH, 1);
+        String health = "Unknown";
+        switch (status5) {
+            case BatteryManager.BATTERY_HEALTH_UNKNOWN:
+                health = "Unknown";
+                break;
+            case BatteryManager.BATTERY_HEALTH_GOOD:
+                health = "Good";
+                break;
+            case BatteryManager.BATTERY_HEALTH_OVERHEAT:
+                health = "Overheat";
+                break;
+            case BatteryManager.BATTERY_HEALTH_DEAD:
+                health = "Dead";
+                break;
+            case BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE:
+                health = "Over voltage";
+                break;
+            case BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE:
+                health = "Unspecified failure";
+                break;
+            case BatteryManager.BATTERY_HEALTH_COLD:
+                health = "Cold";
+                break;
+        }
+
+
+        final ArrayList<String[]> battery = new ArrayList<>();
+        battery.add(new String[]{"Temperature", temperature+"°C"});
+        battery.add(new String[]{"Voltage", voltage+"Volts"});
+        battery.add(new String[]{"Capacity", level+"/"+scale});
+        battery.add(new String[]{"Status", status2+"/"});
+        battery.add(new String[]{"Health", health});
+        battery.add(new String[]{"Total Battery Capacity", getBatteryCapacity()});
+
+
+        ArrayAdapter<String[]> arrayAdapter = new ArrayAdapter<String []>(this,
+                android.R.layout.simple_list_item_2,
+                android.R.id.text1,
+                battery){
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+                TextView text2 = (TextView) view.findViewById(android.R.id.text2);
+
+                String[] entry = battery.get(position);
+                text1.setText(entry[0]);
+                text2.setText(entry[1]);
+
+                return view;
+            }
+        };
+
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(arrayAdapter);
+    }
+
+    public String getBatteryCapacity() {
+        Object mPowerProfile_ = null;
+
+        final String POWER_PROFILE_CLASS = "com.android.internal.os.PowerProfile";
+
+        try {
+            mPowerProfile_ = Class.forName(POWER_PROFILE_CLASS)
+                    .getConstructor(Context.class).newInstance(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            double batteryCapacity = (Double) Class
+                    .forName(POWER_PROFILE_CLASS)
+                    .getMethod("getAveragePower", java.lang.String.class)
+                    .invoke(mPowerProfile_, "battery.capacity");
+
+            return batteryCapacity + " mah";
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "No data";
+
+    }
+}
--- src/main/java/com/example/device_info/MainActivity.java
+++ src/main/java/com/example/device_info/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.device_info;
 
+import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,40 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final ArrayList<String[]> list = new ArrayList<>();
-        list.add(new String[]{"CODENAME", Build.VERSION.CODENAME});
-        list.add(new String[]{"RELEASE", Build.VERSION.RELEASE});
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            list.add(new String[]{"SECURITY_PATCH", Build.VERSION.SECURITY_PATCH});
-        }
-        list.add(new String[]{"The user-visible SDK version of the framework", Integer.toString(Build.VERSION.SDK_INT)});
-        list.add(new String[]{"The name of the underlying board", Build.BOARD});
-        list.add(new String[]{"A build ID string meant for displaying to the user", Build.DISPLAY});
-        list.add(new String[]{"BOOTLOADER", Build.BOOTLOADER});
-        
+    }
 
 
-        ArrayAdapter<String[]> arrayAdapter = new ArrayAdapter<String []>(this,
-                android.R.layout.simple_list_item_2,
-                android.R.id.text1,
-                list){
+    public void aboutBattery(View view) {
+        Intent intent = new Intent(this, BatteryActivity.class);
+        startActivity(intent);
+    }
 
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                View view = super.getView(position, convertView, parent);
-                TextView text1 = (TextView) view.findViewById(android.R.id.text1);
-                TextView text2 = (TextView) view.findViewById(android.R.id.text2);
-
-                String[] entry = list.get(position);
-                text1.setText(entry[0]);
-                text2.setText(entry[1]);
-
-                return view;
-            }
-        };
-
-        ListView listView = (ListView) findViewById(R.id.list);
-        listView.setAdapter(arrayAdapter);
+    public void aboutSystem(View view) {
+        Intent intent = new Intent(this, SystemActivity.class);
+        startActivity(intent);
     }
 }
