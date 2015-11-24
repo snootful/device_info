5ccf18feb36c418ee8a0cbc7feca8d429d888b07
John Doe <goodbyecrazy@gmail.com>
2015-11-24 20:24:44 +0800

    Add something about system information.

--- src/main/java/com/example/device_info/SystemActivity.java
+++ src/main/java/com/example/device_info/SystemActivity.java
@@ -3,15 +3,23 @@ package com.example.device_info;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SystemActivity extends AppCompatActivity {
+
+    private static String  LOG_TAG = "SystemActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,10 @@ public class SystemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_system);
 
         final ArrayList<String[]> list = new ArrayList<>();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            list.add(new String[]{"The kernel version ", getFormattedKernelVersion()});
+        }
+        list.add(new String[]{"A build ID string meant for displaying to the user", Build.DISPLAY});
         list.add(new String[]{"CODENAME", Build.VERSION.CODENAME});
         list.add(new String[]{"RELEASE", Build.VERSION.RELEASE});
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
@@ -26,10 +38,7 @@ public class SystemActivity extends AppCompatActivity {
         }
         list.add(new String[]{"The user-visible SDK version of the framework", Integer.toString(Build.VERSION.SDK_INT)});
         list.add(new String[]{"The name of the underlying board", Build.BOARD});
-        list.add(new String[]{"A build ID string meant for displaying to the user", Build.DISPLAY});
         list.add(new String[]{"BOOTLOADER", Build.BOOTLOADER});
-
-
 
         ArrayAdapter<String[]> arrayAdapter = new ArrayAdapter<String []>(this,
                 android.R.layout.simple_list_item_2,
@@ -57,5 +66,61 @@ public class SystemActivity extends AppCompatActivity {
     @Override
     public void onUserLeaveHint () {
         finish();
+    }
+
+    /**
+     * Reads a line from the specified file.
+     * @param filename the file to read from
+     * @return the first line, if any.
+     * @throws IOException if the file couldn't be read
+     */
+    private static String readLine(String filename) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename), 256)) {
+            return reader.readLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    public static String getFormattedKernelVersion() {
+        try {
+            return formatKernelVersion(readLine("/proc/version"));
+
+        } catch (IOException e) {
+            Log.e(LOG_TAG,
+                    "IO Exception when getting kernel version for Device Info screen",
+                    e);
+
+            return "Unavailable";
+        }
+    }
+
+    public static String formatKernelVersion(String rawKernelVersion) {
+        // Example (see tests for more):
+        // Linux version 3.0.31-g6fb96c9 (android-build@xxx.xxx.xxx.xxx.com) \
+        //     (gcc version 4.6.x-xxx 20120106 (prerelease) (GCC) ) #1 SMP PREEMPT \
+        //     Thu Jun 28 11:02:39 PDT 2012
+
+        final String PROC_VERSION_REGEX =
+                "Linux version (\\S+) " + /* group 1: "3.0.31-g6fb96c9" */
+                        "\\((\\S+?)\\) " +        /* group 2: "x@y.com" (kernel builder) */
+                        "(?:\\(gcc.+? \\)) " +    /* ignore: GCC version information */
+                        "(#\\d+) " +              /* group 3: "#1" */
+                        "(?:.*?)?" +              /* ignore: optional SMP, PREEMPT, and any CONFIG_FLAGS */
+                        "((Sun|Mon|Tue|Wed|Thu|Fri|Sat).+)"; /* group 4: "Thu Jun 28 11:02:39 PDT 2012" */
+
+        Matcher m = Pattern.compile(PROC_VERSION_REGEX).matcher(rawKernelVersion);
+        if (!m.matches()) {
+            Log.e(LOG_TAG, "Regex did not match on /proc/version: " + rawKernelVersion);
+            return "Unavailable";
+        } else if (m.groupCount() < 4) {
+            Log.e(LOG_TAG, "Regex match on /proc/version only returned " + m.groupCount()
+                    + " groups");
+            return "Unavailable";
+        }
+        return m.group(1) + "\n" +                 // 3.0.31-g6fb96c9
+                m.group(2) + " " + m.group(3) + "\n" + // x@y.com #1
+                m.group(4);                            // Thu Jun 28 11:02:39 PDT 2012
     }
 }
