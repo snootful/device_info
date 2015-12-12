be996633c042a9bd09c25d20550f2de80f08c723
Skyline <goodbyecrazy@gmail.com>
2015-12-12 23:22:13 +0800

    Add something about wifi.

--- src/main/java/com/example/device_info/WifiActivity.java
+++ src/main/java/com/example/device_info/WifiActivity.java
@@ -1,7 +1,17 @@
 package com.example.device_info;
 
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class WifiActivity extends AppCompatActivity {
 
@@ -9,5 +19,49 @@ public class WifiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi);
+
+        final ArrayList<String[]> list = new ArrayList<>();
+
+        String macStr = "";
+        WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        if (wifiInfo.getMacAddress() != null) {
+            macStr = wifiInfo.getMacAddress();
+        } else {
+            macStr = "null";
+        }
+        list.add(new String[]{"Mac address", macStr});
+
+        if (!wifiManager.isWifiEnabled()) {
+            wifiManager.setWifiEnabled(true);
+        }
+        int ipAddress = wifiInfo.getIpAddress();
+        list.add(new String[]{"Ip address", (ipAddress & 0xFF) + "." +
+                ((ipAddress >> 8) & 0xFF) + "." +
+                ((ipAddress >> 16) & 0xFF) + "." +
+                (ipAddress >> 24 & 0xFF)});
+
+
+        ArrayAdapter<String[]> arrayAdapter = new ArrayAdapter<String[]>(this,
+                android.R.layout.simple_list_item_2,
+                android.R.id.text1,
+                list) {
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+                TextView text2 = (TextView) view.findViewById(android.R.id.text2);
+
+                String[] entry = list.get(position);
+                text1.setText(entry[0]);
+                text2.setText(entry[1]);
+
+                return view;
+            }
+        };
+
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(arrayAdapter);
     }
 }
